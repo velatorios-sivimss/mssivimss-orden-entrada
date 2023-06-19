1b7391b78113d7f88ad9a0eca2a475a78ec1a13c
METRO\raul.arteaga <raul.arteaga@people-media.com.mx>
2023-06-19 11:00:29 -0600

    Se modifica consulta para obtener los numeros de contrato

--- src/test/java/com/imss/sivimss/ordenEntrada/ArquetipoApplicationTests.java
+++ src/test/java/com/imss/sivimss/ordenEntrada/ArquetipoApplicationTests.java
@@ -1,16 +1,13 @@
 package com.imss.sivimss.ordenEntrada;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import com.imss.sivimss.orden.entrada.OrdenEntradaApplication;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+
 class ArquetipoApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 		String result="test";
 		OrdenEntradaApplication.main(new String[]{});
